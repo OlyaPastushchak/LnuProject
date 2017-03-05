2023b097b7f145e4b022d7405e860d587cabb226
OlyaPastushchak <olyapastuschak@gmail.com>
2017-03-05 21:57:45 +0200

    version for commit

---
+++ AUTH/Exit.h
@@ -0,0 +1,9 @@
+#pragma once
+void exit()
+{
+	system("cls");
+	cout << "\t\nAll information has been successfully saved. \n ";
+	cout << " \t\nThank you for choosing our products .\n\n\n";
+	system("pause");
+	exit(0);
+}
